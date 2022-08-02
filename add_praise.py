3c2a176b3f0571aa822c6b31cad7f0ff61950ede
Lex <lex.kosha@gmail.com>
2022-08-02 09:03:47 +0300

    corrected remark of  func create_commendation

--- add_praise.py
+++ add_praise.py
@@ -14,25 +14,24 @@ def create_commendation(full_name, subject):
     :return:
     """
     try:
-        student = Schoolkid.objects.filter(full_name__contains=full_name).get()
-        lesson = Lesson.objects.filter(
-            year_of_study=student.year_of_study,
-            group_letter=student.group_letter,
-            subject__title=subject
-        ).first()
-
-        commend_text = [
-            'Сказано здорово – просто и ясно!', 'С каждым разом у тебя получается всё лучше!', 'Страна гордится тобой',
-            'Ты будущие России!', 'Не зря тебя мама родила!', f'Ты добился великих достижений по предмету {subject}'
-        ]
-        random_commend = random.choice(commend_text)
-
-        Commendation.objects.create(
-            text=random_commend, created=lesson.date,
-            schoolkid_id=student.pk, subject_id=lesson.subject_id,
-            teacher_id=lesson.teacher.pk)
+        student = Schoolkid.objects(full_name__contains=full_name).get()
     except ObjectDoesNotExist:
         print('ФИО не найдены в базе')
     except MultipleObjectsReturned:
         print(f'Найдено более одного {full_name}, добавьте фамилию и отчество')
+    lesson = Lesson.objects.filter(
+        year_of_study=student.year_of_study,
+        group_letter=student.group_letter,
+        subject__title=subject
+    ).first()
+
+    commend_text = [
+        'Сказано здорово – просто и ясно!', 'С каждым разом у тебя получается всё лучше!', 'Страна гордится тобой',
+        'Ты будущие России!', 'Не зря тебя мама родила!', f'Ты добился великих достижений по предмету {subject}']
+    random_commend = random.choice(commend_text)
+    Commendation.objects.create(
+        text=random_commend, created=lesson.date,
+        schoolkid_id=student.pk, subject_id=lesson.subject_id,
+        teacher_id=lesson.teacher.pk)
+
 
